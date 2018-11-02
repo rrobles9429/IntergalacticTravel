3a86e1742dd03ab4b0f5c064f4bfc4e21433375c
Rebecca Robles <rrobles9429@jh-mba158-17.d214.org>
2018-11-02 11:15:25 -0500

    added class

---
+++ IntergalacticTravel/StarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StarViewController.swift
+//  IntergalacticTravel
+//
+//  Created by Rebecca Robles on 11/2/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class StarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
