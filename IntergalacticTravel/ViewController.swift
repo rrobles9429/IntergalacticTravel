90ef0ab7e3a6dd57f772ed44854c0fb0de4a232e
Rebecca Robles <rrobles9429@jh-mba158-17.d214.org>
2018-11-06 11:38:24 -0600

    added if statement

--- IntergalacticTravel/ViewController.swift
+++ IntergalacticTravel/ViewController.swift
@@ -22,8 +22,15 @@ class ViewController: UIViewController {
 
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let buttonThatTriggered = sender as! UIButton
+        let title = buttonThatTriggered.titleLabel?.text
         let nvc = segue.destination as! StarViewController
-        nvc.title = "Cool"
+        nvc.title = title
+        if title == "Red Dwarf" {
+            nvc.isRedDwarf = true
+        }else{
+            nvc.isRedDwarf = false
+        }
     }
     }
 
